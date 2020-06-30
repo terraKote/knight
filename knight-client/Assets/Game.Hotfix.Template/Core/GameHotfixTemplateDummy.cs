9fcce993e8f3754424768962f7e21acfc0d069bb
winddyhe <hgplan@126.com>
2020-06-30 16:45:33 +0800

    Remove unused code.

--- knight-client/Assets/Game.Hotfix.Template/Core/GameHotfixTemplateDummy.cs
+++ knight-client/Assets/Game.Hotfix.Template/Core/GameHotfixTemplateDummy.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class GameHotfixTemplateDummy : MonoBehaviour
+namespace Game.Hotfix
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameHotfixTemplateDummy
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
